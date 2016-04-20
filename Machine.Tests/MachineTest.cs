8688bc3847375b878f5df30f067d616c13a954e4
BuLLeTCode <raivis.rugelis@va.lv>
2016-04-20 19:00:08 +0300

    Rework with Unit tests

---
+++ Machine.Tests/MachineTest.cs
@@ -0,0 +1,320 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaivisVendingMachine;
+using System;
+
+namespace Machine.Tests
+{
+    [TestClass]
+    public class MachineTest
+    {
+
+        VendingMachine machine;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            //By default, machine with one product
+            machine = new VendingMachine();
+
+            machine.AddNewProduct(2, "Fanta", 1, 9);
+        }
+
+        //Test method when user first time insert money
+        [TestMethod]
+        public void InsertOneEuro()
+        {
+            //Arrange
+            //VendingMachine machine = new VendingMachine();
+
+            //Money what will be inserted - act
+            Money m1 = new Money();
+            m1.Euros = 1;
+            m1.Cents = 0;
+
+            Money vendingMoney = machine.InsertCoin(m1);
+            //Assert
+            Assert.AreEqual(1, vendingMoney.Euros);
+            Assert.AreEqual(0, vendingMoney.Cents);
+        }
+
+        //User inserts no valid coin
+        [TestMethod]
+        public void InsertNotValidCoin()
+        {
+            //Invalid cent
+            Money m1 = new Money();
+            m1.Cents = 12;
+            machine.InsertCoin(m1);
+            //Machine doesnt accept - so, no increase should be in Machine money amount
+            Assert.AreEqual(0, machine.Amount.Cents);
+            
+            //Invalid euro
+            Money m2 = new Money();
+            m2.Euros = 3;
+            machine.InsertCoin(m2);
+
+            Assert.AreEqual(0, machine.Amount.Euros);
+        }
+
+        //User inserts not valid coin after inserting valid coin
+        [TestMethod]
+        public void InsertNotValidAfterValidCoin()
+        {
+            //Invalid cent
+            Money m1 = new Money();
+            m1.Cents = 10;
+            machine.InsertCoin(m1);
+
+            //Invalid euro
+            Money m2 = new Money();
+            m2.Euros = 3;
+            machine.InsertCoin(m2);
+            //Check machine amount after one valid and one not valid coin
+            Assert.AreEqual(10, machine.Amount.Cents);
+            Assert.AreEqual(0, machine.Amount.Euros);
+        }
+
+        [TestMethod]
+        public void InsertTwoCoins_OneEuroAndTenCents()
+        {
+            //Money what will be inserted - act
+            Money m1 = new Money();
+            m1.Euros = 1;
+            m1.Cents = 0;
+
+            Money m2 = new Money();
+            m2.Euros = 0;
+            m2.Cents = 10;
+
+            machine.InsertCoin(m1);
+            Money vendingMoney = machine.InsertCoin(m2);
+            //Assert
+            Assert.AreEqual(1, vendingMoney.Euros);
+            Assert.AreEqual(10, vendingMoney.Cents);
+        }
+
+        [TestMethod]
+        public void ReturnInserted_OneEuroTenCents()
+        {
+            //Money what will be inserted - act
+            Money m1 = new Money();
+            m1.Euros = 1;
+            m1.Cents = 10;
+
+            machine.InsertCoin(m1);
+
+            Money returnMoneyToUser = machine.ReturnMoney();
+            Assert.AreEqual(1, returnMoneyToUser.Euros);
+            Assert.AreEqual(10, returnMoneyToUser.Cents);
+        }
+
+        [TestMethod]
+        public void AddNewProduct_Fanta()
+        {
+            machine.AddNewProduct(1, "Snicker", 0, 67);
+
+            //Check last product in machine product list
+            Assert.AreEqual(1, machine.Products[machine.Products.Length - 1].Available);
+            Assert.AreEqual("Snicker", machine.Products[machine.Products.Length - 1].Name);
+            Assert.AreEqual(0, machine.Products[machine.Products.Length - 1].Price.Euros);
+            Assert.AreEqual(67, machine.Products[machine.Products.Length - 1].Price.Cents);
+            //Check if first product is stil in list
+            Assert.AreEqual("Fanta", machine.Products[0].Name);
+        }
+
+        [TestMethod]
+        public void AddNewProductBeforeAndAfterReturnMoney()
+        {
+            machine.AddNewProduct(1, "Snicker", 0, 67);
+
+            //Check last product in machine product list
+            Assert.AreEqual(2, machine.Products.Length);
+
+            machine.ReturnMoney();
+
+            machine.AddNewProduct(2, "Orbit", 0, 23);
+            Assert.AreEqual(3, machine.Products.Length);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+        "Currency or avaibility cant be negative")]
+        public void TryToAddNotValidNewProduct()
+        {
+            //available amount for new product is 0
+            machine.AddNewProduct(0, "Snicker", 0, 67);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+        "Currency or avaibility cant be negative")]
+        public void TryToAddNotValidNewProduct2()
+        {
+            //negative price
+            machine.AddNewProduct(2, "Snicker", 0, -67);
+        }
+
+        [TestMethod]
+        public void DeleteProductFromProductList()
+        {
+            //Add one product
+            machine.AddNewProduct(1, "Snicker", 0, 67);
+            //Try to delete first product from list
+            machine.DeleteProductFromVendingMachine(1);
+            //Check if one product left and if its new product.
+            Assert.AreEqual(1, machine.Products.Length);
+            Assert.AreEqual("Snicker", machine.Products[0].Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException),
+        "Product is not find")]
+        public void TryToDeleteProductWhatNoExist()
+        {
+            //By default one product is avaible, try to delete second
+            //what not exist
+            machine.DeleteProductFromVendingMachine(2);
+            Assert.AreEqual(1, machine.Products.Length);
+        }
+
+        [TestMethod]
+        public void TryToUpdateFirstProduct()
+        {
+            //Update first - default product availability
+            machine.UpdateProductInProductList(1, 10, "Fanta", 1, 9);
+
+            Assert.AreEqual(10, machine.Products[0].Available);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException),
+        "Product is not find")]
+        public void TryToUpdateProductWhatNotExist()
+        {
+            //Product 99 not exist, try to update it
+            machine.UpdateProductInProductList(99, 5, "SS", 10, 90);
+
+            Assert.AreEqual(5, machine.Products[98].Available);
+        }
+
+        [TestMethod]
+        public void UserWantsToBuyAFanta()
+        {
+            //add Fanta to Product list
+            machine.AddNewProduct(2, "Snicker", 0, 63);
+
+            //user adds money
+            Money m1 = new Money();
+            m1.Euros = 1;
+            m1.Cents = 10;
+
+            machine.InsertCoin(m1);
+            Product machineGive = machine.Buy(2);//Want to buy 
+            Assert.AreEqual(2, machine.Products.Length);
+            Assert.AreEqual("Snicker", machineGive.Name);
+            Assert.AreEqual(1, machineGive.Available);//check if availability is less for one unit
+
+            //Give remainder back 
+            Money remainderAfterBuy = machine.lastRemainder;
+            Assert.AreEqual(0, remainderAfterBuy.Euros);
+            Assert.AreEqual(47, remainderAfterBuy.Cents);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException),
+        "Product is not find")]
+        public void UserWantsToBuyProductWhatDoNoExist()
+        {
+            //Add money
+            Money m1 = new Money();
+            m1.Cents = 20;
+
+            machine.InsertCoin(m1);
+            //Try to buy product what no exist, for example, with number 55
+            Product machineGive = machine.Buy(55);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException),
+        "Product you try to buy is not avaible")]
+        public void UserWantsToBuyProductWhatIsNotAvaible()
+        {
+            machine.AddNewProduct(1, "Mentos", 0, 32);
+            //With money
+            Money m1 = new Money();
+            m1.Cents = 50;
+            machine.InsertCoin(m1);
+            machine.Buy(2);
+            //Buy product what is not avaible
+            machine.InsertCoin(m1);
+            machine.Buy(2);
+        }
+
+        [TestMethod]
+        public void FewUsersBuyDifferentProducts()
+        {
+            //Add two more products to machine
+            machine.AddNewProduct(2, "Orbit", 0, 39);
+            machine.AddNewProduct(5, "Twix", 0, 73);
+
+            //first user adds money
+            Money m1 = new Money();
+            m1.Cents = 50;
+
+            machine.InsertCoin(m1);
+            Product machineGive = machine.Buy(2);//Want to buy second product from 3
+            Assert.AreEqual(3, machine.Products.Length);
+            Assert.AreEqual("Orbit", machineGive.Name);
+            Assert.AreEqual(1, machineGive.Available);//check if availability is less for one unit
+
+            //Give remainder back 
+            Money remainderAfterBuy = machine.lastRemainder;
+            Assert.AreEqual(0, remainderAfterBuy.Euros);
+            Assert.AreEqual(11, remainderAfterBuy.Cents);
+
+            //Next user buys last product, check, how dat goes
+            Money m2 = new Money();
+            m2.Euros = 1;
+
+            machine.InsertCoin(m2);
+            Product machineGiveSecond = machine.Buy(3);//Wants to buy last product
+            Assert.AreEqual(3, machine.Products.Length);
+            Assert.AreEqual("Twix", machineGiveSecond.Name);
+            Assert.AreEqual(4, machineGiveSecond.Available);//check if availability is less for one unit
+
+            //Give remainder back 
+            remainderAfterBuy = machine.lastRemainder;
+            Assert.AreEqual(0, remainderAfterBuy.Euros);
+            Assert.AreEqual(27, remainderAfterBuy.Cents);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException),
+       "Not enough money for buying product")]
+        public void UserWantsToBuyProductWithoutEnoughMoney()
+        {
+            //No money inserted - just buy
+            machine.Buy(1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException),
+       "Not enough money for buying product")]
+        public void UserWantsToBuyProductWithoutEnoughMoney2()
+        {
+            //Not enough money inserted
+            Money m1 = new Money();
+            m1.Cents = 10;
+            machine.InsertCoin(m1);
+
+            machine.Buy(1);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()//Little cleanup
+        {
+            machine = null;
+        }
+
+    }
+}
---
+++ RaivisVendingMachine/Machine.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Linq;
+
+namespace RaivisVendingMachine
+{
+    public interface IVendingMachine
+    {
+        /// <summary>Vending machine manufacturer.</summary>
+        string Manufacturer { get; }
+        /// <summary>Amount of money inserted into vending machine. </summary>
+        Money Amount { get; }
+        /// <summary>Products that are sold.</summary>
+        Product[] Products { get; set; }
+        /// <summary>Inserts the coin into vending machine.</summary>
+        /// <param name="amount">Coin amount.</param>
+        Money InsertCoin(Money amount);
+        /// <summary>Returns all inserted coins back to user.</summary>
+        Money ReturnMoney();
+        /// <summary>Buys product from list of product.</summary>
+        /// <param name="productNumber">Product number in vending machine productlist.</param>
+        Product Buy(int productNumber);
+    }
+    /// <summary> Money describes Euros and cents </summary>
+    public struct Money
+    {
+        public int Euros { get; set; }
+        public int Cents { get; set; }
+    }
+
+    public struct Product
+    {
+        /// <summary>Gets or sets the available amount of product.</summary>
+        public int Available { get; set; }
+        /// <summary>Gets or sets the product price.</summary>
+        public Money Price { get; set; }
+        /// <summary>Gets or sets the product name.</summary>
+        public string Name { get; set; }
+    }
+    //Vending  machine class to store and add logic to IVendingMachine
+    public class VendingMachine : IVendingMachine
+    {
+        //Variables
+        Product[] products = new Product[] { };//Store products for specific VendingMachine object
+        string manufacturer = "Raivis";
+        Money totalBilance = new Money();
+        //Variable what stores last remainder - to check, if is valide
+        public Money lastRemainder = new Money();
+        //-----
+        public Money Amount
+        {
+            get { return totalBilance; }
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return manufacturer;
+            }
+        }
+
+        public Product[] Products
+        {
+            get
+            {
+                return products;
+            }
+
+            set
+            {
+                products = value;
+            }
+        }
+
+        public Product Buy(int productNumber)
+        {
+            //Working with array - product number will be -1
+            int realProductNumber = productNumber - 1;
+
+            IsProductAvaible(realProductNumber);
+
+            //First check, if this product is avaible
+            if (Products[realProductNumber].Available > 0)
+            {
+                //Now check, if user have enough money to buy this product
+                if (((Amount.Euros * 100) + Amount.Cents) >= ((Products[realProductNumber].Price.Euros * 100) + Products[realProductNumber].Price.Cents))
+                {
+                    //--Give product and cash out coin remainder
+                    //One product less 
+                    Products[realProductNumber].Available -= 1;
+                    //Remainder after buy
+                    decimal totalCentsLeft = ((Amount.Euros * 100) + Amount.Cents) - ((Products[realProductNumber].Price.Euros * 100) + Products[realProductNumber].Price.Cents);
+                    int euroLeft = (int)Math.Floor(totalCentsLeft / 100);
+                    int centsLeft = (int)totalCentsLeft % 100;
+                    //Set new bilance
+                    totalBilance.Euros = euroLeft;
+                    totalBilance.Cents = centsLeft;
+                    //Return money
+                    ReturnMoney();
+
+                    //Give product to user
+                    return Products[realProductNumber];
+                }
+                else
+                {
+                    //Info user about not enough coins
+                    ReturnMoney(); //Maybe there is not need to drop out money, if user has not enough money, can buy something else
+                    throw NewInvalidOperationException("Not enough money for buying product");
+                }
+            }
+            else
+            {
+                //Not enough products
+                ReturnMoney();
+                throw NewIndexOutOfRangeException("Product you try to buy is not avaible");
+            }
+        }
+
+        public Money InsertCoin(Money amount)
+        {
+            //Increase user total bilance
+            //Machine accepts only specific coins
+            if (amount.Cents == 5 || amount.Cents == 10 || amount.Cents == 20 || amount.Cents == 50)
+            {
+                totalBilance.Cents += amount.Cents;
+            }
+
+            if (amount.Euros == 1 || amount.Euros == 2)
+            {
+                totalBilance.Euros += amount.Euros;
+            }
+
+            return totalBilance;
+        }
+
+        public Money ReturnMoney()
+        {
+            //Return money and set remainder
+            lastRemainder = totalBilance; 
+
+            totalBilance.Euros = 0;
+            totalBilance.Cents = 0;
+
+            return lastRemainder;
+        }
+        //--Updating Product list
+        //Function to add new product to product list
+        public void AddNewProduct(int avaibility, string productName, int euro, int cents)
+        {
+            //New product
+            Product newProduct = new Product();
+            newProduct.Available = avaibility;
+            newProduct.Name = productName;
+            //Price for product
+            Money newProductPrice = new Money();
+
+            if (euro >= 0 && cents >= 0 && avaibility > 0)
+            {
+                newProductPrice.Euros = euro;
+                newProductPrice.Cents = cents;
+            }
+            else
+            {
+                throw NewArgumentOutOfRangeException("Currency or avaibility cant be negative");
+            }
+
+            newProduct.Price = newProductPrice;
+            //Define array - what will store all products what is now store and new product
+            Product[] updateArray = new Product[Products.Length + 1];
+            //Copy old product
+            for (int i = 0; i < Products.Length; i++)
+            {
+                updateArray[i] = Products[i];
+            }
+            //Add new product to this array
+            updateArray[Products.Length] = newProduct;
+            //Change vending machine product array to new one
+            Products = updateArray;
+        }
+
+        //Function to update product in product list
+        public void UpdateProductInProductList(int productNumber, int avaibility, string productName, int euro, int cents)
+        {
+            int productNumberInArray = productNumber - 1;
+
+            IsProductAvaible(productNumberInArray);
+
+            Product[] updateArray = new Product[Products.Length];
+            //Copy old product - if find Update product, change it
+            for (int i = 0; i < Products.Length; i++)
+            {
+                if(i == productNumberInArray)
+                {
+                    Products[i].Available = avaibility;
+                    Products[i].Name = productName;
+
+                    Money newPrice = new Money();
+                    newPrice.Euros = euro;
+                    newPrice.Cents = cents;
+
+                    Products[i].Price = newPrice;
+
+                    updateArray[i] = Products[i];
+                }
+                else
+                {
+                    updateArray[i] = Products[i];
+                }
+            }
+            //Change vending machine product array to new one
+            Products = updateArray;
+            
+        }
+        //To delete Product from list
+        public void DeleteProductFromVendingMachine(int productNumber)
+        {
+            //Check if avaible
+            IsProductAvaible(productNumber - 1);
+            //Convert array to list
+            var numbersList = Products.ToList();
+            //Remove element with specific index
+            numbersList.RemoveAt(productNumber - 1);
+            //Convert back and assign to Products
+            Products = numbersList.ToArray();
+        }
+        //--end of product list updating functions
+
+        //Little function to check, if product is avaible
+        void IsProductAvaible(int productNumber)
+        {
+            if (productNumber < 0 || productNumber > Products.Length-1)
+            {
+                throw NewIndexOutOfRangeException("Product is not find");
+            }
+        }
+
+        //Different type of exceptions -----------
+        IndexOutOfRangeException NewIndexOutOfRangeException(string message)
+        {
+            return new IndexOutOfRangeException(message);
+        }
+
+        InvalidOperationException NewInvalidOperationException(string message)
+        {
+            return new InvalidOperationException(message);
+        }
+
+        ArgumentOutOfRangeException NewArgumentOutOfRangeException(string message)
+        {
+            return new ArgumentOutOfRangeException(message);
+        }
+        //--Excepion end 
+    }
+}
